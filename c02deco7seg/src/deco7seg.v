10218f2ce1e288dd361048639d1cc0df57ba7c21
Pablo <pablinza@me.com>
2025-07-06 00:22:19 -0400

    Add files via upload

--- c02deco7seg/src/deco7seg.v
+++ c02deco7seg/src/deco7seg.v
@@ -2,11 +2,11 @@
  *Orden de los segmentos <g><f><e><d><c><b><a>*/
 module deco7seg(en, bcd, seg);
 input en; //Habilitador de cambio 
-input [3:0] bcd;     //Entradas del numero binario
-output reg [6:0]seg;//Salidas a los segmentos
+input [3:0] bcd;  //Entradas del numero binario
+output reg [6:0] seg; //Salidas a los segmentos
 always @(*)   //Lista de eventos
 if(en == 1) //Cambio solo con habilitacion
-   case(bcd) //Salida gfedcba según numero bcd
+  case(bcd) //Salida de segmento según numero bcd
      4'b0000: seg = 7'b100_0000; //numero 0
      4'b0001: seg = 7'b111_1001; //numero 1
      4'b0010: seg = 7'b010_0100; //numero 2
@@ -19,8 +19,8 @@ if(en == 1) //Cambio solo con habilitacion
      4'b1001: seg = 7'b001_1000; //numero 9
      4'b1010: seg = 7'b000_1000; //letra  A
      4'b1011: seg = 7'b000_0011; //letra  b
-     4'b1100: seg = 7'b011_0110; //letra  C
-     4'b1101: seg = 7'b101_1110; //letra  d
+     4'b1100: seg = 7'b100_0110; //letra  C
+     4'b1101: seg = 7'b010_0001; //letra  d
      4'b1110: seg = 7'b000_0110; //letra  E
      4'b1111: seg = 7'b000_1110; //letra  F
   endcase
